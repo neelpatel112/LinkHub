8d7f0c43a10865831939ccfcda0572e39e421da5
Neel patel <neel48148@gmail.com>
2026-02-26 20:42:52 +0530

    Create Link model with schema for MongoDB

---
+++ models/Link.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const LinkSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    default: 'link'
+  },
+  order: {
+    type: Number,
+    default: 0
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  clicks: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Link', LinkSchema);
